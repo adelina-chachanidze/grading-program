50bf89dc528b49067851bb868445acd73656cba3
Adelina <154084420+adelina-chachanidze@users.noreply.github.com>
2024-09-05 21:57:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function getAverage(scores) {
-  let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
-  }
-
-  return sum / scores.length;
-}
 function getGrade(score) {
 
   if (score === 100) 
